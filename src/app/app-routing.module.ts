85fe99ab0fb617a4d4a6544061bc484b4f485ebb
Oliver Meyer <meyeroliver9401@gmail.com>
2022-07-17 11:52:06 +0200

    refactoring the customers components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {SettingsComponent} from "./settings/settings.component";
 import {CompanyComponent} from "./company/company.component";
-import {CompanyDetailsTabComponent} from "./company-details-tab/company-details-tab.component";
+import {CompanyDetailsTabComponent} from "./company/company-details-tab/company-details-tab.component";
 
 const routes: Routes = [
   {path: 'home', component: DashboardComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppBarComponent } from './app-bar/app-bar.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { SettingsComponent } from './settings/settings.component';
-import { CompanyListComponent } from './company-list/company-list.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { CompanyComponent } from './company/company.component';
-import { CompanyDetailsTabComponent } from './company-details-tab/company-details-tab.component';
-import {MatTabsModule} from "@angular/material/tabs";
-import { CompanyFormComponent } from './company-form/company-form.component';
-import { SupplierFormComponent } from './supplier-form/supplier-form.component';
-import { CustomerFormComponent } from './customer-form/customer-form.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import { MatRadioModule } from '@angular/material/radio';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import {MatTabsModule} from "@angular/material/tabs";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppBarComponent } from './app-bar/app-bar.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SettingsComponent } from './settings/settings.component';
+import { CompanyComponent } from './company/company.component';
+import { SupplierFormComponent } from './company/supplier-form/supplier-form.component';
+import { CustomerFormComponent } from './company/customer-form/customer-form.component';
+import {CompanyDetailsTabComponent} from "./company/company-details-tab/company-details-tab.component";
+import {CompanyFormComponent} from "./company/company-form/company-form.component";
+import {CompanyListComponent} from "./company/company-list/company-list.component";
 
 @NgModule({
   declarations: [
