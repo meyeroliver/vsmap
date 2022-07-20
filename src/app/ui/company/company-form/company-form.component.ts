cefc465fca89106be926d02834bca0d1b353155f
Oliver Meyer <meyeroliver9401@gmail.com>
2022-07-20 11:30:49 +0200

    refactored the form objects in the component and in the template

--- src/app/ui/company/company-form/company-form.component.ts
+++ src/app/ui/company/company-form/company-form.component.ts
@@ -14,57 +14,68 @@ export class CompanyFormComponent implements OnInit {
   company?: Company;
 
   constructor(private companyService: CompanyService) {
-    this.companyService.companySelected.subscribe((company)=>{
-     this.company = company;
-     this.populateForm();
+    this.companyService.companySelected.subscribe((company) => {
+      this.company = company;
+      this.populateForm();
     })
   }
 
   ngOnInit(): void {
     this.setupForm();
-    if (this.companyService.getSelectedCompany() !== null){
+    if (this.companyService.getSelectedCompany() !== null) {
       this.company = this.companyService.getSelectedCompany();
       this.populateForm();
     }
   }
 
-  private setupForm(){
+  private setupForm() {
     this.companyForm = new FormGroup<any>({
       'companyName': new FormControl(null, Validators.required),
       'vatNumber': new FormControl(null, Validators.required),
       'staffCount': new FormControl(null, Validators.required),
-      'industry': new FormControl(null),
-      'businessType': new FormControl(null),
-      'production': new FormControl(null),
-      'ownerFirstname': new FormControl(null, Validators.required),
-      'ownerLastname': new FormControl(null,Validators.required),
-      'ownerEmail': new FormControl(null, [Validators.required, Validators.email]),
-      'ownerCell': new FormControl(null, [Validators.required, Validators.min(10), Validators.max(12)]),
-      'contactPersonFirstname': new FormControl(null, Validators.required),
-      'contactPersonLastname': new FormControl(null, Validators.required),
-      'contactPersonEmail': new FormControl(null, [Validators.required, Validators.email]),
-      'contactPersonCell': new FormControl(null, [Validators.required, Validators.min(10), Validators.max(12)]),
+      'env': new FormGroup<any>({
+        'industry': new FormControl(null),
+        'businessType': new FormControl(null),
+        'production': new FormControl(null),
+      }),
+      'owner': new FormGroup<any>({
+        'firstname': new FormControl(null, Validators.required),
+        'lastname': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'cell': new FormControl(null, [Validators.required, Validators.min(10), Validators.max(12)]),
+      }),
+      'contactPerson': new FormGroup<any>({
+        'firstname': new FormControl(null, Validators.required),
+        'lastname': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'cell': new FormControl(null, [Validators.required, Validators.min(10), Validators.max(12)]),
+      }),
+
 
     })
   }
 
-  populateForm(){
+  populateForm() {
 
     this.companyForm.patchValue({
         'companyName': this.company?.name,
-      'vatNumber': this.company?.vatNumber,
-      'staffCount': this.company?.staffCount,
-      'industry': this.company?.industry,
-      'businessType': this.company?.businessType,
-      'production': this.company?.production,
-      'ownerFirstname': this.company?.owner?.firstName,
-      'ownerLastname': this.company?.owner?.lastName,
-      'ownerEmail': this.company?.owner?.email,
-      'ownerCell': this.company?.owner?.cell,
-      'contactPersonFirstname': this.company?.contactPerson?.firstName,
-      'contactPersonLastname': this.company?.contactPerson?.lastName,
-      'contactPersonEmail': this.company?.contactPerson?.email,
-      'contactPersonCell': this.company?.contactPerson?.cell
+        'vatNumber': this.company?.vatNumber,
+        'staffCount': this.company?.staffCount,
+        'env.industry': this.company?.industry,
+        'env.businessType': this.company?.businessType,
+        'env.production': this.company?.production,
+        'owner': {
+          'firstname': this.company?.owner?.firstName,
+          'lastname': this.company?.owner?.lastName,
+          'email': this.company?.owner?.email,
+          'cell': this.company?.owner?.cell,
+        },
+        'contactPerson': {
+          'firstname': this.company?.contactPerson?.firstName,
+          'lastname': this.company?.contactPerson?.lastName,
+          'email': this.company?.contactPerson?.email,
+          'cell': this.company?.contactPerson?.cell
+        }
       }
     );
   }
