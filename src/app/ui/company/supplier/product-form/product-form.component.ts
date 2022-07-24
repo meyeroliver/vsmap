8880d431f1015e715fab5e7bd9cd31a57ec7e589
Oliver Meyer <meyeroliver9401@gmail.com>
2022-07-24 10:01:00 +0200

    mock functionalityo the product form

--- src/app/ui/company/supplier/product-form/product-form.component.ts
+++ src/app/ui/company/supplier/product-form/product-form.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {Forms} from "../../../../interfaces";
 
 @Component({
   selector: 'product-form',
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.scss']
 })
-export class ProductFormComponent implements OnInit {
+export class ProductFormComponent implements OnInit, Forms {
 
-  constructor() { }
+  newForm: boolean;
+  editable: boolean;
+  saveable: boolean;
 
-  ngOnInit(): void {
+  constructor() {
+    this.newForm = this.editable = this.saveable = true;
   }
 
+  ngOnInit(): void {
+
+  }
+
+  populateForm(): void {
+  }
+
+  setupForm(): void {
+  }
+
+  onEditClicked() {
+    this.editable = !this.editable;
+    this.saveable = !this.saveable;
+    alert('Still under construction')
+
+  }
+
+  onSaveClicked() {
+    alert('Still under construction')
+    this.onEditClicked();
+  }
 }
